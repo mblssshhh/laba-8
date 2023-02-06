6c367c54231c91bad878756feb550ebd448d21cc
mblss <mblss@LAPTOP-3O6QFSTQ>
2023-02-06 11:50:57 +0300

    Добавьте файлы проекта.

---
+++ laba 8/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace laba_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.RectagleCheck = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(575, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "x:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(574, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "y:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(540, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "weight: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(540, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "height:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(578, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(597, 75);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 22);
+            this.textBoxX.TabIndex = 6;
+            this.textBoxX.TextChanged += new System.EventHandler(this.textBoxX_TextChanged);
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(597, 119);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 22);
+            this.textBoxY.TabIndex = 7;
+            this.textBoxY.TextChanged += new System.EventHandler(this.textBoxY_TextChanged);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(149, 119);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox.TabIndex = 8;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // RectagleCheck
+            // 
+            this.RectagleCheck.AutoSize = true;
+            this.RectagleCheck.Location = new System.Drawing.Point(597, 36);
+            this.RectagleCheck.Name = "RectagleCheck";
+            this.RectagleCheck.Size = new System.Drawing.Size(84, 20);
+            this.RectagleCheck.TabIndex = 9;
+            this.RectagleCheck.Text = "Rectagle";
+            this.RectagleCheck.UseVisualStyleBackColor = true;
+            this.RectagleCheck.CheckedChanged += new System.EventHandler(this.RectagleCheck_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RectagleCheck);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.CheckBox RectagleCheck;
+    }
+}
+
---
+++ laba 8/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba_8
+{
+
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap; // = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+        Pen pen; // = new Pen(Color.Black, 5);
+        static class Init
+        {
+            public static Bitmap bitmap;
+            public static PictureBox pictuteBox;
+            public static Pen pen;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            this.bitmap = new Bitmap(pictureBox.ClientSize.Width, pictureBox.ClientSize.Height);
+            this.pen = new Pen(Color.Black, 5);
+            Init.bitmap = this.bitmap;
+            Init.pictuteBox = pictureBox;
+            Init.pen = this.pen;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        abstract public class Figure
+        {
+            public int x;
+            public int y;
+            public int w;
+            public int h;
+
+           
+            public void Draw()
+            {
+                
+            }
+
+            public void MoveTo(int x, int y)
+            {
+
+            }
+        }
+
+        //first level
+        public class Rectagle : Figure
+        {
+            public void DrawRectangle(System.Drawing.Pen pen)
+            {
+                Graphics g = Graphics.FromImage(Init.bitmap);
+                g.DrawRectangle(pen, w, h, w, h);
+                Init.pictuteBox.Image = Init.bitmap;
+            }
+        }
+
+        public class Elips : Figure
+        {
+            public void Draw()
+            {
+
+            }
+        }
+
+        public class Polygon : Figure
+        {
+
+        }
+
+        //second level
+        public class Circle : Elips
+        {
+            
+        }
+
+        public class Square : Rectagle
+        {
+
+        }
+
+        public class Triangle : Polygon
+        {
+
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (RectagleCheck.Checked)
+            {
+
+            }
+        }
+
+        private void textBoxX_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxY_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RectagleCheck_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
